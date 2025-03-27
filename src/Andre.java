2a6de74ed02d4ad6093cbf8397e7470ae55c8e07
Nyagrenen <113024289+Nyagrenen@users.noreply.github.com>
2025-03-27 09:57:56 +0100

    Update Andre.java
    lasagne2

--- src/Andre.java
+++ src/Andre.java
@@ -1,3 +1,10 @@
-public void food() {
+public class Andre{
+	public void hello(){
+		System.out.println("Hej jag är André");
+	}
+
+	public void food() {
         System.out.println("Lasagne");
     }
+
+}
